53a2865660af76e13f72c91db9f5032b4ca167c9
myfiles01 <anilkumar509n@gmail.com>
2024-07-30 16:55:44 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://WNASUB:vanshux7@wnasub.nengxhz.mongodb.net/?retryWrites=true&w=majority&appName=WNASUB'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1001968083700'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'WNASUB'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
